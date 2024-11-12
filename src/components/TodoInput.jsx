e1256cec12faa03db734b91f4d0d5595d21006ca
lawrencedulian <lawrenceedulian@gmail.com>
2024-11-12 13:08:24 +0100

    little type

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Todoinput(props) {
+export default function TodoInput(props) {
     const { handleAddTodos, todoValue, setTodoValue } = props
 
 
